78b70042937012b7af845ea44a94e784eed45017
Jinny0077 <leongjiayan@gmail.com>
2025-08-12 21:26:02 +0800

    update

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,33 +1,26 @@
-import React from "react";
 import { getImageUrl } from "../../utils";
 import styles from "./ProjectCard.module.css";
 
-export const ProjectCard = ({
-  project: { title, imageSrc, description, skills, source },
-}) => {
+export const ProjectCard = ({ project: { title, imageSrc, skills, demo } }) => {
   return (
-    <div className={styles.container}>
-      <img
-        src={getImageUrl(imageSrc)}
-        alt={`Image of ${title}`}
-        className={styles.image}
-      />
-      <h3 className={styles.title}>{title}</h3>
-      <p className={styles.description}>{description}</p>
-      <ul className={styles.skills}>
-        {skills.map((skill, id) => {
-          return (
-            <li key={id} className={styles.skill}>
-              {skill}
-            </li>
-          );
-        })}
-      </ul>
-      <div className={styles.links}>
-        <a href={source} className={styles.link}>
-          Source
-        </a>
+    <a href={demo} target="_blank" rel="noopener noreferrer" className={styles.projectLink}>
+      <div className={styles.container}>
+        <img
+          src={getImageUrl(imageSrc)}
+          alt={`Image of ${title}`}
+          className={styles.image}
+        />
+        <h3 className={styles.title}>{title}</h3>
+        <ul className={styles.skills}>
+          {skills.map((skill, id) => {
+            return (
+              <li key={id} className={styles.skill}>
+                {skill}
+              </li>
+            );
+          })}
+        </ul>
       </div>
-    </div>
+    </a>
   );
 };
